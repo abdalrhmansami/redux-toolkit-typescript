ffd4884ed9004d5ce0217eae8a2e7a1e2b10871f
12580390 <abdalrhmansami5@gmail.com>
2022-10-06 11:49:59 +0300

    add reaction project

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import {
-  JSXElementConstructor,
-  ReactElement,
-  ReactFragment,
-  ReactPortal,
-  useCallback,
-  useEffect,
-} from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { useGameQuery, useGamesQuery } from "./services/gamesApi";
+import PostList from "./features/posts/PostList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
-  const { data, error, isLoading, isFetching, isSuccess } = useGamesQuery();
   return (
-    <div>
-      <h1>React Redux toolkit RTK</h1>
-      {isLoading && <h2>Loading...</h2>}
-      {isFetching && <h2>isFetching...</h2>}
-      {error && <h2>Something wrong...</h2>}
-      {isSuccess && (
-        <div>
-          {data.map((game: any) => (
-            <div className="data">
-              <span>{game.name}</span>
-              <GameDetail id={game.id} />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <main className="app">
+      <AddPostForm />
+      <PostList />
+    </main>
   );
 }
-
-export const GameDetail = ({ id }: { id: string }) => {
-  const { data } = useGameQuery(id);
-  return <div>{JSON.stringify(data, undefined, 2)}</div>;
-};
 
 export default App;
--- src/models/game.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Game {
-  name: string;
-  address: string;
-  numberOfPeople: number;
-  date: Date;
-  time: string;
-  fieldNumber: number;
-}
